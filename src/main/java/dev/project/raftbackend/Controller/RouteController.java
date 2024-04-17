1ad6b601c84555f2d05adfae58eb2db7da72ce79
Prasoon Mishra <95595435+HackvistPrasoon@users.noreply.github.com>
2024-04-17 16:37:17 +0530

    Update RouteController.java

--- src/main/java/dev/project/raftbackend/Controller/RouteController.java
+++ src/main/java/dev/project/raftbackend/Controller/RouteController.java
@@ -339,15 +339,17 @@ public class RouteController {
              return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
          }
     }
-    
+	
     @CrossOrigin(origins = "*")
-    @PostMapping("api/preview")
+    @PostMapping("api/previewapp")
     public ResponseEntity<?>  preview(HttpServletRequest request, @RequestBody UserDetailsBean userdetails1) {
         try {
+        	System.out.println("Preview 1 "+userdetails1.getToken());
         	String email = cookieService.getEmailId(userdetails1.getToken());
+        	System.out.println("Email"+userdetails1.getToken());
             Userdetails userdetails = getUserByEmail(email).getBody();
             System.out.println("Preview");
-            System.out.println(userdetails1);
+            System.out.println(userdetails);
             UserDetailsPreview udp = new UserDetailsPreview();
             assert userdetails != null;
             udp.setEmailid(userdetails.getEmailid());
@@ -361,6 +363,9 @@ public class RouteController {
             udp.setRiver_states(river.getStates());
             udp.setRiver_date(river.getDate());
             udp.setOutside_my_bubble(userdetails.getOutside_my_bubble());
+            udp.setOmb_first_name(userdetails.getOmb_first_name());
+            udp.setOmb_last_name(userdetails.getOmb_last_name());
+            udp.setOmb_emailid(userdetails.getOmb_emailid());
             Ethnic eth = ethnicRepo.getEthnic(userdetails.getEthnic_id());
             udp.setEthnicType(eth.getEthnicType());
             Gender gen = genderRepo.getGender(userdetails.getGender_id());
@@ -371,40 +376,231 @@ public class RouteController {
             udp.setAgeGroup(age.getAgeGroup());
             Location loc = locRepo.getLocation(userdetails.getLocation_id());
             udp.setLocation(loc.getLocation());
-            udp.setConservative(userdetails.getConservative());
-            udp.setMaga(userdetails.getMaga());
-            udp.setLibertarian(userdetails.getLibertarian());
-            udp.setModerate(userdetails.getModerate());
-            udp.setLiberal(userdetails.getLiberal());
-            udp.setProgressive(userdetails.getProgressive());
-            udp.setSocialist(userdetails.getSocialist());
-            udp.setApolitical(userdetails.getApolitical());
-            udp.setDemocrat(userdetails.getDemocrat());
-            udp.setRepublican(userdetails.getRepublican());
-            udp.setLiberatarian_political_party(userdetails.getLiberatarian_political_party());
-            udp.setGreen(userdetails.getGreen());
-            udp.setIndependent(userdetails.getIndependent());
-            udp.setOther(userdetails.getOther());
+            
+            if(userdetails.getConservative().equalsIgnoreCase("A")) {
+            	udp.setConservative("Always Agree");
+            } else if(userdetails.getConservative().equalsIgnoreCase("FA")) {
+            	udp.setConservative("Frequently Agree");
+            } else if(userdetails.getConservative().equalsIgnoreCase("N")) {
+            	udp.setConservative("Neutral");
+            } else if(userdetails.getConservative().equalsIgnoreCase("FD")) {
+            	udp.setConservative("Frequently Disagree");
+            } else if(userdetails.getConservative().equalsIgnoreCase("AD")) {
+            	udp.setConservative("Always Disagree");
+            }
+            
+            if(userdetails.getMaga().equalsIgnoreCase("AA")) {
+            	udp.setMaga("Always Agree");
+            } else if(userdetails.getMaga().equalsIgnoreCase("FA")) {
+            	udp.setMaga("Frequently Agree");
+            } else if(userdetails.getMaga().equalsIgnoreCase("N")) {
+            	udp.setMaga("Neutral");
+            } else if(userdetails.getMaga().equalsIgnoreCase("FD")) {
+            	udp.setMaga("Frequently Disagree");
+            } else if(userdetails.getMaga().equalsIgnoreCase("AD")) {
+            	udp.setMaga("Always Disagree");
+            }
+            
+            if(userdetails.getLibertarian().equalsIgnoreCase("AA")) {
+            	udp.setLibertarian("Always Agree");
+            } else if(userdetails.getLibertarian().equalsIgnoreCase("FA")) {
+            	udp.setLibertarian("Frequently Agree");
+            } else if(userdetails.getLibertarian().equalsIgnoreCase("River_N")) {
+            	udp.setLibertarian("Neutral");
+            } else if(userdetails.getLibertarian().equalsIgnoreCase("FD")) {
+            	udp.setLibertarian("Frequently Disagree");
+            } else if(userdetails.getLibertarian().equalsIgnoreCase("AD")) {
+            	udp.setLibertarian("Always Disagree");
+            }
+            
+            if(userdetails.getModerate().equalsIgnoreCase("AA")) {
+            	udp.setModerate("Always Agree");
+            } else if(userdetails.getModerate().equalsIgnoreCase("FA")) {
+            	udp.setModerate("Frequently Agree");
+            } else if(userdetails.getModerate().equalsIgnoreCase("N")) {
+            	udp.setModerate("Neutral");
+            } else if(userdetails.getModerate().equalsIgnoreCase("FD")) {
+            	udp.setModerate("Frequently Disagree");
+            } else if(userdetails.getModerate().equalsIgnoreCase("AD")) {
+            	udp.setModerate("Always Disagree");
+            }
+            
+            if(userdetails.getLiberal().equalsIgnoreCase("AA")) {
+            	udp.setLiberal("Always Agree");
+            } else if(userdetails.getLiberal().equalsIgnoreCase("FA")) {
+            	udp.setLiberal("Frequently Agree");
+            } else if(userdetails.getLiberal().equalsIgnoreCase("N")) {
+            	udp.setLiberal("Neutral");
+            } else if(userdetails.getLiberal().equalsIgnoreCase("FD")) {
+            	udp.setLiberal("Frequently Disagree");
+            } else if(userdetails.getLiberal().equalsIgnoreCase("AD")) {
+            	udp.setLiberal("Always Disagree");
+            }
+            
+            if(userdetails.getProgressive().equalsIgnoreCase("AA")) {
+            	udp.setProgressive("Always Agree");
+            } else if(userdetails.getProgressive().equalsIgnoreCase("FA")) {
+            	udp.setProgressive("Frequently Agree");
+            } else if(userdetails.getProgressive().equalsIgnoreCase("N")) {
+            	udp.setProgressive("Neutral");
+            } else if(userdetails.getProgressive().equalsIgnoreCase("FD")) {
+            	udp.setProgressive("Frequently Disagree");
+            } else if(userdetails.getProgressive().equalsIgnoreCase("AD")) {
+            	udp.setProgressive("Always Disagree");
+            }
+           
+            if(userdetails.getSocialist().equalsIgnoreCase("AA")) {
+            	udp.setSocialist("Always Agree");
+            } else if(userdetails.getSocialist().equalsIgnoreCase("FA")) {
+            	udp.setSocialist("Frequently Agree");
+            } else if(userdetails.getSocialist().equalsIgnoreCase("N")) {
+            	udp.setSocialist("Neutral");
+            } else if(userdetails.getSocialist().equalsIgnoreCase("FD")) {
+            	udp.setSocialist("Frequently Disagree");
+            } else if(userdetails.getSocialist().equalsIgnoreCase("AD")) {
+            	udp.setSocialist("Always Disagree");
+            }
+            
+            if(userdetails.getApolitical().equalsIgnoreCase("AA")) {
+            	udp.setApolitical("Always Agree");
+            } else if(userdetails.getApolitical().equalsIgnoreCase("FA")) {
+            	udp.setApolitical("Frequently Agree");
+            } else if(userdetails.getApolitical().equalsIgnoreCase("N")) {
+            	udp.setApolitical("Neutral");
+            } else if(userdetails.getApolitical().equalsIgnoreCase("FD")) {
+            	udp.setApolitical("Frequently Disagree");
+            } else if(userdetails.getApolitical().equalsIgnoreCase("AD")) {
+            	udp.setApolitical("Always Disagree");
+            }
+           
+            if(userdetails.getDemocrat().equalsIgnoreCase("AA")) {
+            	udp.setDemocrat("Always Agree");
+            } else if(userdetails.getDemocrat().equalsIgnoreCase("FA")) {
+            	udp.setDemocrat("Frequently Agree");
+            } else if(userdetails.getDemocrat().equalsIgnoreCase("N")) {
+            	udp.setDemocrat("Neutral");
+            } else if(userdetails.getDemocrat().equalsIgnoreCase("FD")) {
+            	udp.setDemocrat("Frequently Disagree");
+            } else if(userdetails.getDemocrat().equalsIgnoreCase("AD")) {
+            	udp.setDemocrat("Always Disagree");
+            }
+            
+            if(userdetails.getRepublican().equalsIgnoreCase("AA")) {
+            	udp.setRepublican("Always Agree");
+            } else if(userdetails.getRepublican().equalsIgnoreCase("FA")) {
+            	udp.setRepublican("Frequently Agree");
+            } else if(userdetails.getRepublican().equalsIgnoreCase("N")) {
+            	udp.setRepublican("Neutral");
+            } else if(userdetails.getRepublican().equalsIgnoreCase("FD")) {
+            	udp.setRepublican("Frequently Disagree");
+            } else if(userdetails.getRepublican().equalsIgnoreCase("AD")) {
+            	udp.setRepublican("Always Disagree");
+            }
+            
+            if(userdetails.getLiberatarian_political_party().equalsIgnoreCase("AA")) {
+            	udp.setLiberatarian_political_party("Always Agree");
+            } else if(userdetails.getLiberatarian_political_party().equalsIgnoreCase("FA")) {
+            	udp.setLiberatarian_political_party("Frequently Agree");
+            } else if(userdetails.getLiberatarian_political_party().equalsIgnoreCase("N")) {
+            	udp.setLiberatarian_political_party("Neutral");
+            } else if(userdetails.getLiberatarian_political_party().equalsIgnoreCase("FD")) {
+            	udp.setLiberatarian_political_party("Frequently Disagree");
+            } else if(userdetails.getLiberatarian_political_party().equalsIgnoreCase("AD")) {
+            	udp.setLiberatarian_political_party("Always Disagree");
+            }
+           
+            if(userdetails.getGreen().equalsIgnoreCase("AA")) {
+            	udp.setGreen("Always Agree");
+            } else if(userdetails.getGreen().equalsIgnoreCase("FA")) {
+            	udp.setGreen("Frequently Agree");
+            } else if(userdetails.getGreen().equalsIgnoreCase("N")) {
+            	udp.setGreen("Neutral");
+            } else if(userdetails.getGreen().equalsIgnoreCase("FD")) {
+            	udp.setGreen("Frequently Disagree");
+            } else if(userdetails.getGreen().equalsIgnoreCase("AD")) {
+            	udp.setGreen("Always Disagree");
+            }
+            
+            if(userdetails.getIndependent().equalsIgnoreCase("AA")) {
+            	udp.setIndependent("Always Agree");
+            } else if(userdetails.getIndependent().equalsIgnoreCase("FA")) {
+            	udp.setIndependent("Frequently Agree");
+            } else if(userdetails.getIndependent().equalsIgnoreCase("N")) {
+            	udp.setIndependent("Neutral");
+            } else if(userdetails.getIndependent().equalsIgnoreCase("FD")) {
+            	udp.setIndependent("Frequently Disagree");
+            } else if(userdetails.getIndependent().equalsIgnoreCase("AD")) {
+            	udp.setIndependent("Always Disagree");
+            }
+            
+            if(userdetails.getOther().equalsIgnoreCase("AA")) {
+            	udp.setOther("Always Agree");
+            } else if(userdetails.getOther().equalsIgnoreCase("FA")) {
+            	udp.setOther("Frequently Agree");
+            } else if(userdetails.getOther().equalsIgnoreCase("N")) {
+            	udp.setOther("Neutral");
+            } else if(userdetails.getOther().equalsIgnoreCase("FD")) {
+            	udp.setOther("Frequently Disagree");
+            } else if(userdetails.getOther().equalsIgnoreCase("AD")) {
+            	udp.setOther("Always Disagree");
+            }
+            
+            
+           
             udp.setText_box1(userdetails.getText_box1());
             udp.setText_box2(userdetails.getText_box2());
             udp.setRadio_comfort(userdetails.getRadio_comfort());
+            
+            if(userdetails.getRadio_comfort()<=3) {
+            	udp.setRadio_comfort_string("Very Unfavourable");
+            } else if(userdetails.getRadio_comfort()>4 || userdetails.getRadio_comfort()<=7) {
+            	udp.setRadio_comfort_string("Neutral");
+            }else {
+            	udp.setRadio_comfort_string("Very Favourable");
+            }
+            
             udp.setRadio_feel(userdetails.getRadio_feel());
+            
+            if(userdetails.getRadio_feel()<=3) {
+            	udp.setRadio_feel_string("Very Unfavourable");
+            } else if(userdetails.getRadio_feel()>4 || userdetails.getRadio_feel()<=7) {
+            	udp.setRadio_feel_string("Neutral");
+            }else {
+            	udp.setRadio_feel_string("Very Favourable");
+            }
+            
             udp.setRadio_pov(userdetails.getRadio_pov());
+            
+            if(userdetails.getRadio_pov()<=3) {
+            	udp.setRadio_pov_string("Very Unfavourable");
+            } else if(userdetails.getRadio_pov()>4 || userdetails.getRadio_pov()<=7) {
+            	udp.setRadio_pov_string("Neutral");
+            }else {
+            	udp.setRadio_pov_string("Very Favourable");
+            }
+            
             udp.setRadio_prespective(userdetails.getRadio_prespective());
             
+            if(userdetails.getRadio_prespective()<=3) {
+            	udp.setRadio_prespective_string("Very Unfavourable");
+            } else if(userdetails.getRadio_prespective()>4 || userdetails.getRadio_prespective()<=7) {
+            	udp.setRadio_prespective_string("Neutral");
+            }else {
+            	udp.setRadio_prespective_string("Very Favourable");
+            }
+            
+            udp.setToken(userdetails1.getToken());
+            udp.setStatus("ok");
+            
             TabMapping tabs = mappingRepo.findByEmailId(email);
-            tabs.setTab5("true");
+            tabs.setTab6("true");
             mappingRepo.saveAndFlush(tabs);
             
-            ResponseData responseData = new ResponseData();
-	        responseData.setStatus("ok");
-	        responseData.setToken(userdetails1.getToken());
-	        responseData.setUdp(udp);
-	        
 	        // Return response with status OK and response object
-	        return ResponseEntity.ok(responseData);
+	        return ResponseEntity.ok(udp);
         }
         catch (Exception e){
+        	System.out.println(e);
             System.out.println("Error");
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
